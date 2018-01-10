7d4585f02b72b2fc2c516978189daaad8913c88e
Paul Chote <p.chote@warwick.ac.uk>
2018-01-10 18:25:49 -0500

    Add daemon entry for superwasp roomalert.

--- warwick/observatory/common/daemons.py
+++ warwick/observatory/common/daemons.py
@@ -68,6 +68,7 @@ tng_log = PyroDaemon('tng_daemon', IP.GOTOServer, 9011, 5)
 goto_roomalert = PyroDaemon('goto_roomalert_daemon', IP.GOTOServer, 9020, 5)
 goto_ups = PyroDaemon('goto_ups_daemon', IP.GOTOServer, 9021, 5)
 goto_vaisala = PyroDaemon('goto_vaisala_daemon', IP.GOTOServer, 9022, 5)
+superwasp_roomalert = PyroDaemon('superwasp_roomalert_daemon', IP.GOTOServer, 9023, 5)
 
 onemetre_operations = PyroDaemon('operations_daemon', IP.OneMetreDome, 9015, 5)
 onemetre_environment = PyroDaemon('environment_daemon', IP.OneMetreDome, 9002, 5)
